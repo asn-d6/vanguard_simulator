7de3d722be269af2169c80750eea5ba998607d56
Mike Perry <mikeperry-git@torproject.org>
2018-04-19 21:28:40 +0000

    Add adversary models to automation.sh

--- automator.sh
+++ automator.sh
@@ -3,8 +3,8 @@
 # This script runs an experiment for each possible configuration.
 # This will take many many hours by default so I suggest you customize it.
 
-sybil_models="weak medium hard"
-pwnage_models="basic APT FVEY"
+sybil_models="weak medium hard hell"
+pwnage_models="basic APT FVEY rubberhose1 rubberhose2"
 topology_models="1-2-4 1-2-6 1-2-8 1-4-4 1-4-6 1-4-8 2-2-4 2-2-6 2-2-8 2-4-4 2-4-6 2-4-8"
 
 for sybil in $sybil_models
