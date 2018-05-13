c247fff2e108c8a42ac23a90e43ebd710674e9b4
Mike Perry <mikeperry-git@torproject.org>
2018-05-13 20:37:56 +0000

    Trim down automator to interesting models.

--- automator.sh
+++ automator.sh
@@ -3,9 +3,9 @@
 # This script runs an experiment for each possible configuration.
 # This will take many many hours by default so I suggest you customize it.
 
-sybil_models="weak medium hard hell"
-pwnage_models="basic APT FVEY rubberhose1 rubberhose2"
-topology_models="1-2-4 1-2-6 1-2-8 1-4-4 1-4-6 1-4-8 2-2-4 2-2-6 2-2-8 2-4-4 2-4-6 2-4-8"
+sybil_models="tiny medium hell"
+pwnage_models="none rubberhose1 rubberhose2"
+topology_models="2-3-4 2-3-6 2-4-6 2-4-8"
 
 for sybil in $sybil_models
 do
